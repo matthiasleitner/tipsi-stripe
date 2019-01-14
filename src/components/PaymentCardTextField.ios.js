789c0be275b84ce1e80d8eb272d6b98c2c0802d0
Matthias Leitner <matthias.leitner@lints.at>
2019-01-14 17:24:57 +0100

    fix prop typeS

--- src/components/PaymentCardTextField.ios.js
+++ src/components/PaymentCardTextField.ios.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
 import { requireNativeComponent, findNodeHandle, StyleSheet, View, TouchableWithoutFeedback } from 'react-native'
 import StyleSheetPropType from 'react-native/Libraries/StyleSheet/StyleSheetPropType'
 import ViewStylePropTypes from 'react-native/Libraries/Components/View/ViewStylePropTypes'
